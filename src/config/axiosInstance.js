7223126dceb15fc46a3e32d163ddba009f9e8d3d
purusoth46 <purusoth910@gmail.com>
2024-10-07 19:46:17 +0800

    first commit

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =  'https://learning-management-system-server.onrender.com/api/v1/'
+const BASE_URL = "https://lms-back-end-1.onrender.com/api/v1/";
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: 10000,
